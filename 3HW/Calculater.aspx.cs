2d0a18ba8b7cebee6190649c15f59e93e99dc625
明輝 陳 <107111210@mail.oit.edu.tw>
2021-04-23 08:47:15 +0800

    final

--- 3HW/Calculater.aspx.cs
+++ 3HW/Calculater.aspx.cs
@@ -7,14 +7,14 @@ using System.Web.UI.WebControls;
 
 namespace _3HW {
     public partial class Calculater : System.Web.UI.Page {
-        
+
         [SerializableAttribute]
         public class NumInfo {
             public int i_BoundCount;
             public int[] ia_List;
             public int i_Operater;
             public NumInfo() {
-                ia_List = new int[2];                
+                ia_List = new int[2];
                 i_BoundCount = 1;
                 i_Operater = 0;
             }
@@ -34,7 +34,8 @@ namespace _3HW {
         }
 
         protected void bt_Add_Click(object sender, EventArgs e) {
-            if (o_Num.i_BoundCount > 1) {
+            if (o_Num.i_BoundCount > 1)
+            {
                 o_Num.ia_List[0] += o_Num.ia_List[1];
                 o_Num.ia_List[1] = 0;
                 o_Num.i_BoundCount--;
@@ -47,15 +48,45 @@ namespace _3HW {
         }
 
         protected void bt_Miuns_Click(object sender, EventArgs e) {
-
+            if (o_Num.i_BoundCount > 1)
+            {
+                o_Num.ia_List[0] += o_Num.ia_List[1];
+                o_Num.ia_List[1] = 0;
+                o_Num.i_BoundCount--;
+            }
+            o_Num.i_Operater = 2;
+            lb_Operate.Text = "-";
+            o_Num.ia_List[1] = 0;
+            o_Num.i_BoundCount++;
+            ViewState["PreivousInfo"] = o_Num;
         }
 
         protected void bt_Product_Click(object sender, EventArgs e) {
-
+            if (o_Num.i_BoundCount > 1)
+            {
+                o_Num.ia_List[0] += o_Num.ia_List[1];
+                o_Num.ia_List[1] = 0;
+                o_Num.i_BoundCount--;
+            }
+            o_Num.i_Operater = 3;
+            lb_Operate.Text = "*";
+            o_Num.ia_List[1] = 0;
+            o_Num.i_BoundCount++;
+            ViewState["PreivousInfo"] = o_Num;
         }
 
         protected void bt_Divide_Click(object sender, EventArgs e) {
-
+            if (o_Num.i_BoundCount > 1)
+            {
+                o_Num.ia_List[0] += o_Num.ia_List[1];
+                o_Num.ia_List[1] = 0;
+                o_Num.i_BoundCount--;
+            }
+            o_Num.i_Operater = 4;
+            lb_Operate.Text = "/";
+            o_Num.ia_List[1] = 0;
+            o_Num.i_BoundCount++;
+            ViewState["PreivousInfo"] = o_Num;
         }
 
         protected void bt_Clear_Click(object sender, EventArgs e) {
@@ -70,19 +101,42 @@ namespace _3HW {
                     if (o_Num.i_BoundCount > 1) {
                         o_Num.ia_List[0] += o_Num.ia_List[1];
                         o_Num.ia_List[1] = 0;
-                        o_Num.i_BoundCount--;                        
+                        o_Num.i_BoundCount--;
+                    }
+                    break;
+                case 2:
+                    if (o_Num.i_BoundCount > 1)
+                    {
+                        o_Num.ia_List[0] -= o_Num.ia_List[1];
+                        o_Num.ia_List[1] = 0;
+                        o_Num.i_BoundCount--;
+                    }
+                    break;
+                case 3:
+                    if (o_Num.i_BoundCount > 1)
+                    {
+                        o_Num.ia_List[0] *= o_Num.ia_List[1];
+                        o_Num.ia_List[1] = 0;
+                        o_Num.i_BoundCount--;
+                    }
+                    break;
+                case 4:
+                    if (o_Num.i_BoundCount > 1)
+                    {
+                        o_Num.ia_List[0] /= o_Num.ia_List[1];
+                        o_Num.ia_List[1] = 0;
+                        o_Num.i_BoundCount--;
                     }
                     break;
                 default:
                     break;
-            }            
+            }
             lb_Operate.Text = "";
             lb_Result.Text = (o_Num.ia_List[0]).ToString();
-
             ViewState["PreivousInfo"] = o_Num = null;
         }
 
-        protected void bt_0_Click(object sender, EventArgs e) {            
+        protected void bt_0_Click(object sender, EventArgs e) {
             if (Convert.ToInt32(lb_Result.Text) != 0) {
                 lb_Result.Text += "0";
                 i_Num = Convert.ToInt32(lb_Result.Text);
@@ -113,38 +167,101 @@ namespace _3HW {
         }
 
         protected void bt_3_Click(object sender, EventArgs e) {
-
+            if (Convert.ToInt32(lb_Result.Text) == 0)
+            {
+                lb_Result.Text = "3";
+            }
+            else
+            {
+                lb_Result.Text += "3";
+            }
+            i_Num = Convert.ToInt32(lb_Result.Text);
+            mt_SetInfo();
         }
 
         protected void bt_4_Click(object sender, EventArgs e) {
-
+            if (Convert.ToInt32(lb_Result.Text) == 0)
+            {
+                lb_Result.Text = "4";
+            }
+            else
+            {
+                lb_Result.Text += "4";
+            }
+            i_Num = Convert.ToInt32(lb_Result.Text);
+            mt_SetInfo();
         }
 
         protected void bt_5_Click(object sender, EventArgs e) {
-
+            if (Convert.ToInt32(lb_Result.Text) == 0)
+            {
+                lb_Result.Text = "5";
+            }
+            else
+            {
+                lb_Result.Text += "5";
+            }
+            i_Num = Convert.ToInt32(lb_Result.Text);
+            mt_SetInfo();
         }
 
         protected void bt_6_Click(object sender, EventArgs e) {
-
+            if (Convert.ToInt32(lb_Result.Text) == 0)
+            {
+                lb_Result.Text = "6";
+            }
+            else
+            {
+                lb_Result.Text += "6";
+            }
+            i_Num = Convert.ToInt32(lb_Result.Text);
+            mt_SetInfo();
         }
 
         protected void bt_7_Click(object sender, EventArgs e) {
-
+            if (Convert.ToInt32(lb_Result.Text) == 0)
+            {
+                lb_Result.Text = "7";
+            }
+            else
+            {
+                lb_Result.Text += "7";
+            }
+            i_Num = Convert.ToInt32(lb_Result.Text);
+            mt_SetInfo();
         }
 
         protected void bt_8_Click(object sender, EventArgs e) {
-
+            if (Convert.ToInt32(lb_Result.Text) == 0)
+            {
+                lb_Result.Text = "8";
+            }
+            else
+            {
+                lb_Result.Text += "8";
+            }
+            i_Num = Convert.ToInt32(lb_Result.Text);
+            mt_SetInfo();
         }
 
         protected void bt_9_Click(object sender, EventArgs e) {
-
+            if (Convert.ToInt32(lb_Result.Text) == 0)
+            {
+                lb_Result.Text = "9";
+            }
+            else
+            {
+                lb_Result.Text += "9";
+            }
+            i_Num = Convert.ToInt32(lb_Result.Text);
+            mt_SetInfo();
         }
 
         private void mt_SetInfo() {
-            int i_Ind = (o_Num.i_BoundCount - 1) ;            
+            int i_Ind = (o_Num.i_BoundCount - 1);
             o_Num.ia_List[i_Ind] = i_Num;
             ViewState["PreivousInfo"] = o_Num;
         }
-
+    
     }
 }
